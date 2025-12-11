b2cb30d18d32c24c6994eba22e87cdf0ccddf0eb
mubasheransari <mubashera38@gmail.com>
2025-12-11 09:34:37 +0500

    updations

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js on MacBook! 🎉');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
